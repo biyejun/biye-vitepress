c516848eeef13127d367d619e07c64031b530a78
biyejun <bnbiye@163.com>
2023-11-10 23:56:44 +0800

    test: add test env

--- dev.js
+++ dev.js
@@ -2,4 +2,7 @@ console.log(process.env.DEV);
 
 console.log(process.argv);
 
+console.log(process.env.DEV, 'process.env.DEV');
+console.log(process.env.aaa, 'process.env.aaa');
+
 console.log('dev js run');
--- rollup.config.js
+++ rollup.config.js
@@ -4,6 +4,14 @@ import { builtinModules, createRequire } from 'module';
 const require = createRequire(import.meta.url)
 const pkg = require('./package.json')
 
+const DEV = !!process.env.DEV
+const PROD = !DEV
+
+console.log(DEV, 'DEV');
+console.log(PROD, 'PROD');
+console.log(process.argv, 'process.argv');
+
+
 const external = [
   ...Object.keys(pkg.dependencies),
   ...Object.keys(pkg.peerDependencies),
@@ -11,6 +19,3 @@ const external = [
     m.includes('punycode') ? [] : [m, `node:${m}`]
   )
 ]
-
-console.log(external, 'external~~~');
-
